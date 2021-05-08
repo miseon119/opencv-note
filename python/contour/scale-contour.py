5002eeec247c0557a38412296d3e3470350a5035
Wendy Park <piaomeixian119@hotmail.com>
2021-05-08 11:54:27 +0900

    Update scale-contour.py

--- python/contour/scale-contour.py
+++ python/contour/scale-contour.py
@@ -1,1 +1,30 @@
-##
+import cv2
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+def scale_contour(cnt, scale):
+    M = cv2.moments(cnt)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+
+    cnt_norm = cnt - [cx, cy]
+    cnt_scaled = cnt_norm * scale
+    cnt_scaled = cnt_scaled + [cx, cy]
+    cnt_scaled = cnt_scaled.astype(np.int32)
+
+    return cnt_scaled
+
+# Reading the image
+im = cv2.imread('star.jpg')
+imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnt_scaled = scale_contour(contours[0], 0.3)
+
+im_copy = im.copy()
+cv2.drawContours(im_copy, contours, 0, (255, 0, 0), 3)
+cv2.drawContours(im_copy, [cnt_scaled], 0, (0, 255, 0), 3)
+cv2.imshow("im_copy",im_copy)
+cv2.waitKey(0)
